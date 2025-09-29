71b7c5d218577a1b108178453408ca3471366c87
syann97 <shallom75321@gmail.com>
2025-09-29 19:36:57 +0900

    init: init msa project

---
+++ apigateway-service/src/test/java/com/github/syann97/toymsa/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syann97.toymsa.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
