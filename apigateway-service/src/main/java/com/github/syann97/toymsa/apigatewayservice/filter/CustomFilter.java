99894c6fa5c82a849f8860af84fb048dbdf00bfc
syann97 <shallom75321@gmail.com>
2025-09-30 12:45:59 +0900

    feat(gateway): 커스텀 필터링 구현 #1

---
+++ apigateway-service/src/main/java/com/github/syann97/toymsa/apigatewayservice/filter/CustomFilter.java
@@ -0,0 +1,38 @@
+package com.github.syann97.toymsa.apigatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+	public CustomFilter() {
+		super(CustomFilter.Config.class);
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange, chain) -> {
+			ServerHttpRequest request = exchange.getRequest();
+			ServerHttpResponse response = exchange.getResponse();
+
+			// Custom Pre Filter
+			log.info("Custom PRE Filter: request id -> {}", request.getId());
+
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				// Custom Post Filter
+				log.info("Custom POST Filter: response code -> {}", response.getStatusCode());
+			}));
+		};
+	}
+
+	public static class Config {
+
+	}
+}
