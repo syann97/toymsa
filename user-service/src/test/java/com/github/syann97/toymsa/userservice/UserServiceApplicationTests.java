774920aaccbd1d27cf6bebb1629f67a6bf29cddf
syann97 <shallom75321@gmail.com>
2025-09-30 17:48:30 +0900

    feat(Users): User Micoservice 프로젝트 생성

---
+++ user-service/src/test/java/com/github/syann97/toymsa/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syann97.toymsa.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
