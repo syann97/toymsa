93aec50805fcfe124797594cf70e707aa9d686a1
syann97 <shallom75321@gmail.com>
2026-02-19 17:53:21 +0900

    feat(Config): user-service 수정 및 Actuator 적용 + Api Gateway에 Config Server 연동 #11

--- user-service/src/main/java/com/github/syann97/toymsa/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/github/syann97/toymsa/userservice/security/WebSecurity.java
@@ -43,10 +43,12 @@ public class WebSecurity {
 		http.csrf( (csrf) -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/h2-console/**").permitAll()  // 특정 경로 허용
+				.requestMatchers("/actuator/**").permitAll()  // 특정 경로 허용
+				.requestMatchers("/health-check/**").permitAll()  // 특정 경로 허용
 				.requestMatchers("/**").access(
 					new WebExpressionAuthorizationManager(
 						"hasIpAddress('127.0.0.1') or hasIpAddress('::1') or " +
-							"hasIpAddress('10.90.26.89') or hasIpAddress('::1')")) // host pc ip address
+							"hasIpAddress('192.168.200.254') or hasIpAddress('::1')")) // host pc ip address
 				.anyRequest().authenticated()              // 그 외는 인증 필요
 			)
 			.authenticationManager(authenticationManager)
