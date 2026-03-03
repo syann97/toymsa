027d5ad4a2855b6ccd0efbd82ffd719faea1e58b
syann97 <shallom75321@gmail.com>
2026-03-03 21:29:45 +0900

    feat(Communication): Feign ErrorDecoder 예외 테스트 정상화 #17

--- user-service/src/main/java/com/github/syann97/toymsa/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/github/syann97/toymsa/userservice/client/OrderServiceClient.java
@@ -11,6 +11,6 @@ import com.github.syann97.toymsa.userservice.dto.ResponseOrder;
 @FeignClient(name="order-service")
 public interface OrderServiceClient {
 
-	@GetMapping("/order-service/{userId}/orders-ng")
+	@GetMapping("/order-service/{userId}/orders")
 	List<ResponseOrder> getOrders(@PathVariable String userId);
 }
